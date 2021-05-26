8e42397524289e7fc11a7e6fa75d6af3e59d08f4
Pedro Javier Nicolás <pedrojavier.nz@gmail.com>
2021-05-26 15:43:09 +0200

    Add linter

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config({ path: '.env' })
 
-let API_PORT = Number(process.env.API_PORT)
-let BOT_TOKEN = process.env.BOT_TOKEN as string
+const API_PORT = Number(process.env.API_PORT)
+const BOT_TOKEN = process.env.BOT_TOKEN as string
 
 if (!Number.isInteger(API_PORT) || API_PORT < 1 || API_PORT > 65535) {
   throw new Error('[ENV] API_PORT must be greater than 1 and lower than 65535')
